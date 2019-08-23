0c234a3dda26f1799170292c264466474b5289d9
juspinas <justin.espinas@gmail.com>
2019-08-23 14:39:20 +1200

    Completed backend

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// D:\OneDrive\GitProjects\myapp
+// D:\OneDrive\GitProjects\random-yugioh-card
 const express = require('express');
+const path = require("path");
 const request = require("request-promise")
 const app = express();
 const port = process.env.PORT || 3000;
+app.use("/static", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.get("/yugioh_card", async (req, res) => {
@@ -16,28 +18,11 @@ app.get("/yugioh_card", async (req, res) => {
         }
     };
     const yugiohResponse = await request(settings).catch(error => console.log(error));
+    const yugiohResponseObj = JSON.parse(yugiohResponse);
     res.status(200).send({
-        SUCCESS: "true",
-        message: "Reached API Successfuly",
-        card: yugiohResponse
-    });
-});
-
-app.get("/yugioh", async (req, res) => {
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: "https://db.ygoprodeck.com/api/v5/cardinfo.php",
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-    const yugiohResponse = await request(settings).catch(error => console.log(error));
-    res.status(200).send({
-        SUCCESS: "true",
-        message: "Reached API Successfuly",
-        cards: yugiohResponse
+        name: yugiohResponseObj[0].name,
+        type: yugiohResponseObj[0].type,
+        image_url: yugiohResponseObj[0].card_images[0].image_url
     });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ public/index.js
@@ -0,0 +1,37 @@
+var cardName
+var cardType
+var cardImage
+var count = 0
+
+$( document ).ready(function() {
+    $("#nextButton").click(function( event ) {
+        console.log("This is from index.js");
+        event.preventDefault()
+        debugger
+        return $.ajax({
+            async: "false",
+            method: "GET",
+            dataType: 'json',
+            url: "/yugioh_card",
+            data: {},
+            success: function (data) {
+                cardName = data.name
+                cardType = data.type
+                cardImage = data.image_url
+                nextCard()
+            }
+        })
+    });
+});
+
+function nextCard() {
+    debugger
+    const Name = cardName
+    const Type = cardType
+    const image_url = cardImage
+    $("#name").html(Name)
+    $("#type").html(Type)
+    $("#image").attr("src", image_url)
+    $("#nextButton").html("Next")
+    count++
+}
